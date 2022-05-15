28938872c744f1d0782c240a8b622be40ecab32a
unknown <saradawsone@gmail.com>
2022-05-15 10:41:14 -0700

    finally got correct text in tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const Tabs = (topics) => {
+  console.log(topics);
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -19,26 +20,31 @@ const Tabs = (topics) => {
   const tab1 = document.createElement('div');
   const tab2 = document.createElement('div');
   const tab3 = document.createElement('div');
+  const tab4 = document.createElement('div');
+  const tab5 = document.createElement('div');
 
   tabContainer.classList.add('topics');
   tab1.classList.add('tab');
   tab2.classList.add('tab');
   tab3.classList.add('tab');
+  tab4.classList.add('tab');
+  tab5.classList.add('tab');
 
-  tab1.textContent = topics[0];
-  tab2.textContent = topics[1];
-  tab3.textContent = topics[2];
+  tab1.textContent = `${topics[0]}`;
+  tab2.textContent = `${topics[1]}`;
+  tab3.textContent = `${topics[2]}`;
+  tab4.textContent = `${topics[3]}
+  `;
+  tab5.textContent = `${topics[4]}`;
+
 
   tabContainer.appendChild(tab1);
   tabContainer.appendChild(tab2);
   tabContainer.appendChild(tab3);
+  tabContainer.appendChild(tab4);
+  tabContainer.appendChild(tab5);
 
   return tabContainer;
-}
-
-const topics = [];
-for (let i = 0; i < topics.length; i++){
-  Tabs(topics[i]);
 }
 
 const tabsAppender = (selector) => {
@@ -49,11 +55,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const tabContainer = document.querySelector(selector);
   axios.get(`http://localhost:5001/api/topics`)
   .then((res) => {
-    // console.log(res.data.topics);
-    document.querySelector(selector).appendChild(Tabs(res.data.topics));
-    topics.push(res.data.topics);
+    let topic = [res.data.topics];
+    console.log(topic);
+    topic.forEach(topics => {
+      tabContainer.appendChild(Tabs(topics));
+    });
   }).catch((err) => {
     console.error(err);
   })
